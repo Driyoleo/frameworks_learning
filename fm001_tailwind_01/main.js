6da506709d83c31d09bd0a51250dbd3f927f63b7
driyo <debashisr433@gmail.com>
2024-03-15 12:42:35 +0530

    tailwind_setup

---
+++ fm001_tailwind_01/main.js
@@ -0,0 +1,12 @@
+
+// 1> npm init -y
+// 2> npm i -D tailwindcss         to install tailwindcss as devdependencies 
+// 2> npx tailwindcss init
+// 3> create src/input.css
+// 4> npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+// 5> `"./src/**/*.{html,js}"
+// 6> include src/output.css in html
+// 7> when all work is done delete module file, input css and upload
+
+
+
